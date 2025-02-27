be8c42011ce016d1575ad2affb4701ccc686a131
Dilip Vengsarkar <shubhankar1313@proton.me>
2025-02-27 20:03:31 +0530

    Update web_script.py

--- web_script.py
+++ web_script.py
@@ -2,7 +2,6 @@ import streamlit as st
 from util import details_to_file_name
 from script import generate_certificate
 from datetime import date
-# import time
 
 st.title("Infosys Certificate Generator")
 
@@ -37,18 +36,3 @@ if st.button("Submit", type="primary"):
                 file_name=f"{certificate}.pdf",
                 mime="application/pdf",
             )
-
-# 'Starting a long computation...'
-
-# # Add a placeholder
-# latest_iteration = st.empty()
-# bar = st.progress(0)
-
-# for i in range(100):
-#   # Update the progress bar with each iteration.
-#   latest_iteration.text(f'Iteration {i+1}')
-#   bar.progress(i + 1)
-#   time.sleep(0.1)
-
-# '...and now we\'re done!'
-
